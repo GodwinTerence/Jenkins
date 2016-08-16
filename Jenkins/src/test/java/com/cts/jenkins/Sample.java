1851a9c4ae4776281fec110d992264190d3b2944
Godwin Terence <godwin.terence@pearson.com>
2016-08-16 20:09:18 +0530

    Push Two

--- Jenkins/src/test/java/com/cts/jenkins/Sample.java
+++ Jenkins/src/test/java/com/cts/jenkins/Sample.java
@@ -1,5 +1,19 @@
 package com.cts.jenkins;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 public class Sample {
 
+	@Test
+	@Parameters("url")
+	public void testMethod(String url)
+	{
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get(url);
+	}
 }

d9b5bdee90c2400f4ca7d720595e5e97e9c6dbcd
Godwin Terence <godwin.terence@pearson.com>
2016-08-18 13:02:23 +0530

    push 5

--- Jenkins/src/test/java/com/cts/jenkins/Sample.java
+++ Jenkins/src/test/java/com/cts/jenkins/Sample.java
@@ -1,11 +1,14 @@
 package com.cts.jenkins;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Parameters;
@@ -19,10 +22,10 @@ public class Sample {
 	public void testMethod() throws MalformedURLException
 	{
 		
-		demo=System.getProperty("url");
-		System.out.println(demo);		
-		WebDriver driver = new FirefoxDriver();
+		File pathToBinary = new File("C:\\Users\\PEARSON-PC\\Downloads\\firefox-47.0.1.win64.sdk\\firefox-sdk\\bin\\firefox.exe");
+		FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+		FirefoxProfile firefoxProfile = new FirefoxProfile();       
+		WebDriver driver = new FirefoxDriver(ffBinary,firefoxProfile);
 		driver.get("http://www.google.com");
-		
 	}
 }

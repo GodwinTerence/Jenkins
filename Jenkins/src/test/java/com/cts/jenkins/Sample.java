64c469f2fa9a9e2e8a1cb22d500ce0590d2df848
Godwin Terence <godwin.terence@pearson.com>
2016-08-25 14:43:09 +0530

    push babay

--- Jenkins/src/test/java/com/cts/jenkins/Sample.java
+++ Jenkins/src/test/java/com/cts/jenkins/Sample.java
@@ -36,7 +36,10 @@ public class Sample {
 	public void testMethod() throws MalformedURLException {
 
 		driver.get("http://www.google.com");
+		System.out.println(
+				driver.findElement(By.xpath("//*[@id='tsf']/div[2]/div[3]/center/input[2]")).getAttribute("value"));
 		driver.findElement(By.xpath("//*[@id='sb_ifc0']")).sendKeys("Godwin");
+
 	}
 
 	@AfterMethod
